5049ca36a1230dbe9f35f4647b784379a34a84be
Bryan Leonard <bryan@forcequit.io>
2025-01-20 15:11:42 -0500

    Fix upload test

--- tests/18-uploads.test.ts
+++ tests/18-uploads.test.ts
@@ -1,6 +1,20 @@
 import { test, expect } from '@playwright/test';
 import fs from 'fs';
 
+test.skip('Download test file', async ({ page }) => {
+
+	await page.goto('/savings.html');
+
+	const downloadPromise = page.waitForEvent('download');
+
+	await page.getByText('Download Our Offer').click();
+
+	const download = await downloadPromise;
+
+	// fails to run because this file opens in the browser rather than being downloaded.
+
+});
+
 test('Download test zip', async ({ page }) => {
 
 	await page.goto('/savings.html');
